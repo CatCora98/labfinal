f814a3acdde407b629a300e227c105a454f615cf
moli1296 <yichendu97@gmail.com>
2019-04-29 23:52:23 -0400

    gonna be ugly..fix bug then, version 3 in progress

--- app/src/androidTest/java/com/temple/edu/bookcase/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/temple/edu/bookcase/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.bookcase;
+package com.temple.edu.bookcase;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.bookcase", appContext.getPackageName());
+        assertEquals("com.temple.edu.bookcase", appContext.getPackageName());
     }
 }
